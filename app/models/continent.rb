91c23943bba682912e3507150eb549811c086fac
Dave King <djking@glam.ac.uk>
2015-04-02 09:43:01 +0100

    tidy up

--- app/models/continent.rb
+++ app/models/continent.rb
@@ -3,5 +3,16 @@ class Continent < ActiveRecord::Base
   
   accepts_nested_attributes_for :countries, :reject_if => :all_blank, allow_destroy: true
  
+ 
+ def destroy
+   # byebug
+  begin
+    return if @_destroy_callback_already_called
+    @_destroy_callback_already_called = true
+    run_callbacks(:destroy) { super }
+  ensure
+    @_destroy_callback_already_called = false
+  end
+end
   
 end
